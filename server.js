09bdf828494cabc54b5444b3defa8042f996cccc
eloflo41 <elo.robert41@gmail.com>
2026-02-24 15:33:08 +0100

    modif .env

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-// Ce bloc va créer un fichier "debug.txt" à la racine de ton site
-try {
-    const express = require('express');
-    const app = express();
-
-    app.get("/health", (req, res) => {
-        res.json({ status: "OK" });
-    });
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        fs.appendFileSync('debug.txt', `Démarrage réussi à ${new Date()}\n`);
-    });
-} catch (err) {
-    // Si ça crash, l'erreur sera écrite ici
-    fs.appendFileSync('debug.txt', `CRASH au démarrage : ${err.message}\n${err.stack}\n`);
-}
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('CONNEXION ETABLIE');
+}).listen(process.env.PORT);

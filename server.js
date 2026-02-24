dbf43854a2f20ca785ae3ba1aa151216697c04a5
eloflo41 <elo.robert41@gmail.com>
2026-02-24 15:05:55 +0100

    modif server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const http = require('http');
-
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('BRAVO ELO : Le serveur Node tourne enfin ! (Test sans Express)');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Vérification réussie : Le moteur Node tourne.');
 });
-
-// Port géré par Plesk
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log("Test de survie actif");
-});
+server.listen(process.env.PORT || 3000);
